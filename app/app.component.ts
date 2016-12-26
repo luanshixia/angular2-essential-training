1be12373927de17902bb419aa0362645e895609c
Yang Wang <luanshixia@gmail.com>
2016-12-25 23:31:52 -0500

    Cleaning

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mw-app',
+  selector: 'my-app',
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
